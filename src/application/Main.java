beefb123289345475bf32ff9693b5034b1994a73
Allan Tiotonho <allanpereira773@gmail.com>
2022-09-26 20:58:37 -0300

    Special move - Castling

--- src/application/Main.java
+++ src/application/Main.java
@@ -34,7 +34,7 @@ public class Main {
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece capturedPiece = chessMatch.peformChessMove(source, target);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
